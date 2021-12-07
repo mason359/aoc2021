cb54102278b8bbbfb7b79d623773b7e0c1d69570
Mason Murphy <mrmurphy888@gmail.com>
2021-12-07 07:03:55 -0800

    Fix bug in day 7

--- day7.py
+++ day7.py
@@ -10,7 +10,7 @@ def problem2():
     crabs = [int(pos) for pos in get_raw(7).split(',')]
     calc_fuel = lambda align: sum(abs(pos - align) * (abs(pos - align) + 1) / 2 for pos in crabs)
     prev = calc_fuel(0)
-    for i in range(1, crabs[-1]):
+    for i in range(1, max(crabs)):
         new = calc_fuel(i)
         if new > prev:
             return int(prev)
