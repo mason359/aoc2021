c4be827b83dec3f512d9b95d9f09d2559f24af3f
Mason Murphy <mrmurphy888@gmail.com>
2021-12-14 09:58:29 -0800

    Complete day 14

---
+++ day14.py
@@ -0,0 +1,35 @@
+from aocutils import get_raw
+
+from collections import defaultdict
+from math import ceil
+
+def problem1():
+    return simulate(10)
+
+def problem2():
+    return simulate(40)
+
+def simulate(steps):
+    mapping, counts = parse_input()
+    for _ in range(steps):
+        counts = defaultdict(int, {pair: sum(counts[starter] for starter in starters) for pair, starters in mapping.items()})
+    return calc_answer(counts)
+
+def parse_input():
+    template, rules = get_raw(14).split('\n\n')
+    mapping = defaultdict(list)
+    for rule in rules.splitlines():
+        pair, insert = rule.split(' -> ')
+        mapping[pair[0] + insert].append(pair)
+        mapping[insert + pair[1]].append(pair)
+    counts = defaultdict(int)
+    for i in range(len(template) - 1):
+        counts[template[i:i + 2]] += 1
+    return mapping, counts
+
+def calc_answer(counts):
+    elements = defaultdict(int)
+    for pair, count in counts.items():
+        elements[pair[0]] += count
+        elements[pair[1]] += count
+    return ceil((max(elements.values()) - min(elements.values())) / 2)
